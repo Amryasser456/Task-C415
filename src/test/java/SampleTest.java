439d15864e5a048c0f49231c09776ec3c7144920
amryasser456 <amryasser3072004@gmail.com>
2025-10-17 23:31:40 +0300

    add junit

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,7 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SampleTest {
 
+    @Test
     void testAddition() {
         int result = 2 + 5;
+        assertEquals(7, result);
     }
 }
