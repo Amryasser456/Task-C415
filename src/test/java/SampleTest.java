eb5216882f0e2c0b579ca544bfd04e5780ec9449
amryasser456 <amryasser3072004@gmail.com>
2025-10-18 00:39:09 +0300

    add calcultor test

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,11 +1,33 @@
+
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SampleTest {
 
+    Calculator calc = new Calculator();
+
     @Test
-    void testAddition() {
-        int result = 2 + 5;
-        assertEquals(7, result);
+    public void testAddition() {
+        assertEquals(8, calc.add(5, 3));
+    }
+
+    @Test
+    public void testSubtraction() {
+        assertEquals(2, calc.subtract(5, 3));
+    }
+
+    @Test
+    public void testMultiplication() {
+        assertEquals(15, calc.multiply(5, 3));
+    }
+
+    @Test
+    public void testDivision() {
+        assertEquals(2.5, calc.divide(5, 2));
+    }
+
+    @Test
+    public void testDivisionByZero() {
+        assertThrows(IllegalArgumentException.class, () -> calc.divide(5, 0));
     }
 }
